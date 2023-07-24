efef94ae8ac1fa2cf4a4b337f0a11a113771710e
wanjiruw <wanjymacharia@gmail.com>
2023-07-24 16:02:13 -0700

    encrypted

---
+++ rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - prints encrypted character in a string
+ * @c: a character
+ * Return: it returns an integer
+ */
+
+int rot13(char *c)
+{
+	int len = strlen(c);
+	char *s = c;
+	char base;
+
+	while (*s)
+	{
+		if (isalpha(*s))
+		{
+			base = _islower(*s) ? 'a' : 'A';
+			_putchar((*s - base + 13) % 26 + base);
+		}
+		else
+			_putchar(*s);
+		s++;
+	}
+	return (len);
+}
