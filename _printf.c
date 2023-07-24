dc599ab9d0c7e07d4674ca598a59ef6799831fb4
wanjiruw <wanjymacharia@gmail.com>
2023-07-23 17:41:20 -0700

    new update

--- _printf.c
+++ _printf.c
@@ -1,57 +1,99 @@
 #include "main.h"
 
 /**
- * _printf - prints format string
- * @format: format string
- * Return: number of characters printed or -1 if there is an error
+ * switch_format - switches format
+ * @c: character
+ * @len: length of string
  */
 
-int _printf(const char *format, ...)
+void switch_format(char *c, int len)
 {
-	int len = 0;
-	va_list format_args;
-
-	va_start(format_args, format);
-	if (format == NULL)
-		return (-1);
-	while (*format)
+	if (*c == '%')
 	{
-		/* looks for format specifier and call appropriate function*/
-		if (*format == '%')
+		c++;
+		switch (*c)
 		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					len += _putchar(va_arg(format_args, int));
-					break;
-				case 's':
-					len += _puts(va_arg(format_args, char *));
-					break;
-				case '%':
-					len += _putchar(*format);
-					break;
-				case '\0':
-					return (-1);
-				case ' ':
-					return (-1);
-				case 'i':
-					len += print_integer(va_arg(format_args, int));
-					break;
-				case 'd':
-					len += print_integer(va_arg(format_args, int));
-					break;
-				default:
-					len += _putchar(*format);
-					break;
-			}
+			case 'c':
+				*len += _putchar(va_arg(format_args, int));
+				break;
+			case 's':
+				*len += _puts(va_arg(format_args, char *));
+				break;
+			case '%':
+				*len += _putchar(*c);
+				break;
+			case '\0':
+				return (-1);
+			case ' ':
+				return (-1);
+			case 'i':
+				*len += print_integer(va_arg(format_args, int));
+				break;
+			case 'd':
+				*len += print_integer(va_arg(format_args, int));
+				break;
+			default:
+				*len += _putchar(*c);
+				break;
 		}
-		else
-		{
-			len += _putchar(*format);
-		}
-		format++;
 	}
-	va_end(format_args);
-	return (len);
+	else
+	{
+		*len += _putchar(*c);
+	}
 }
+	/**
+	 * _printf - prints format string
+	 * @format: format string
+	 * Return: number of characters printed or -1 if there is an error
+	 */
+
+	int _printf(const char *format, ...)
+	{
+		int len = 0;
+		va_list format_args;
+
+		va_start(format_args, format);
+		if (format == NULL)
+			return (-1);
+		while (*format)
+		{
+			/* looks for format specifier and call appropriate function*/
+			if (*format == '%')
+			{
+				format++;
+				switch (*format)
+				{
+					case 'c':
+						len += _putchar(va_arg(format_args, int));
+						break;
+					case 's':
+						len += _puts(va_arg(format_args, char *));
+						break;
+					case '%':
+						len += _putchar(*format);
+						break;
+					case '\0':
+						return (-1);
+					case ' ':
+						return (-1);
+					case 'i':
+						len += print_integer(va_arg(format_args, int));
+						break;
+					case 'd':
+						len += print_integer(va_arg(format_args, int));
+						break;
+					default:
+						len += _putchar(*format);
+						break;
+				}
+			}
+			else
+			{
+				len += _putchar(*format);
+			}
+			format++;
+		}
+		va_end(format_args);
+		return (len);
+	}
