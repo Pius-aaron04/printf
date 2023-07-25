f69e0cb9e1d48a5c4849f64020523e00d8dbebbe
wanjiruw <wanjymacharia@gmail.com>
2023-07-25 15:11:53 -0700

    updated _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -7,7 +7,30 @@
  *
  * -1 on error. errno is set approximately
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: number of character printed
+ */
+
+int _puts(char *str)
+{
+	int l = 0;
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		l++;
+
+	}
+	return (l);
 }
