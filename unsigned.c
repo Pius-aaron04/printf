f51ff63efde607dc97406a5604b9498106da53cb
Pius Aaron <piuschbz@gmail.com>
2023-07-25 21:05:05 +0000

    Revert "Task 0 completion"
    This reverts commit bebe9277beca2f4da468999967716219dc887b74.

---
+++ unsigned.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_unsigned - prints integers
+ * @num: number
+ * Return: lenght of integer
+ */
+
+int print_unsigned(unsigned int num)
+{
+	int len = 0;
+
+	if (num >= 10)
+	{
+		len += print_unsigned(num / 10);
+	}
+	_putchar(num % 10 + '0');
+	len++;
+	return (len);
+}
